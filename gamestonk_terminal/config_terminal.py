6ab7f1ed388a18bd216137b56ea5cffc29df6722
santanshy <63693265+santanshy@users.noreply.github.com>
2021-03-18 17:34:26 +0800

    Update config_terminal.py

--- gamestonk_terminal/config_terminal.py
+++ gamestonk_terminal/config_terminal.py
@@ -1,15 +1,15 @@
 import os
 
 # https://www.alphavantage.co
-API_KEY_ALPHAVANTAGE = os.getenv("GT_API_KEY_ALPHAVANTAGE") or "REPLACE_ME"
+API_KEY_ALPHAVANTAGE = os.getenv("GT_API_KEY_ALPHAVANTAGE") or "29OZ9SC7JZ78A187"
 
 # https://financialmodelingprep.com/developer
 API_KEY_FINANCIALMODELINGPREP = (
-    os.getenv("GT_API_KEY_FINANCIALMODELINGPREP") or "REPLACE_ME"
+    os.getenv("GT_API_KEY_FINANCIALMODELINGPREP") or "74ff445f6114ec80e096a1c16f763ad2"
 )
 
 # https://www.quandl.com/tools/api
-API_KEY_QUANDL = os.getenv("GT_API_KEY_QUANDL") or "REPLACE_ME"
+API_KEY_QUANDL = os.getenv("GT_API_KEY_QUANDL") or "yD_D2tWaa5yJXmmTA4UB"
 
 # https://www.reddit.com/prefs/apps
 API_REDDIT_CLIENT_ID = os.getenv("GT_API_REDDIT_CLIENT_ID") or "REPLACE_ME"
@@ -19,7 +19,7 @@ API_REDDIT_USER_AGENT = os.getenv("GT_API_REDDIT_USER_AGENT") or "REPLACE_ME"
 API_REDDIT_PASSWORD = os.getenv("GT_API_REDDIT_PASSWORD") or "REPLACE_ME"
 
 # https://polygon.io
-API_POLYGON_KEY = os.getenv("GT_API_POLYGON_KEY") or "REPLACE_ME"
+API_POLYGON_KEY = os.getenv("GT_API_POLYGON_KEY") or "y6HttyskyEfcljUko_TlikFpLXQgaUP6"
 
 # https://developer.twitter.com
 API_TWITTER_KEY = os.getenv("GT_API_TWITTER_KEY") or "REPLACE_ME"
@@ -27,4 +27,4 @@ API_TWITTER_SECRET_KEY = os.getenv("GT_API_TWITTER_SECRET_KEY") or "REPLACE_ME"
 API_TWITTER_BEARER_TOKEN = os.getenv("GT_API_TWITTER_BEARER_TOKEN") or "REPLACE_ME"
 
 # https://fred.stlouisfed.org/docs/api/api_key.html
-API_FRED_KEY = os.getenv("GT_FRED_API_KEY") or "REPLACE_ME"
+API_FRED_KEY = os.getenv("GT_FRED_API_KEY") or "51e7afe9599e0a2fad70392842d6646e"
